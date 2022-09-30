30e43f85a46270f0f56cfc7f4a6a3f68b93aad04
tcepeda <tcepeda@pladema.net>
2022-09-30 19:41:55 -0300

    Animaciones en los botones y pantalla de carga

--- Entregable 2/script.js
+++ Entregable 2/script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     let buttonsToggleNavMenu = document.querySelectorAll(".button-toggle-nav-menu-js");
     buttonsToggleNavMenu.forEach(button => button.addEventListener("click", toggleNav));
-    console.log("buttonsToggleNavMenu");
+    loading();
 });
 
 function toggleNav() {
@@ -9,4 +9,22 @@ function toggleNav() {
     let navBackground = document.querySelector("#sidebar-menu-background-js");
     navBackground.classList.toggle("open");
     navMenu.classList.toggle("open");
+}
+
+function loading() {
+    
+    let progress = document.getElementById("progress");
+    let i = 0;
+    let timeProgress = setInterval(() => {
+        if(i <= 100)
+            progress.innerHTML = i++ + "%";
+    }, 48);
+
+    setTimeout(() => {
+        document.getElementById("loading").classList.toggle("loading");
+        document.getElementById("loading").classList.add("none");
+        document.getElementById("nav").classList.toggle("none");
+        document.getElementById("main").classList.toggle("none");
+        clearInterval(timeProgress);
+    }, 5000);
 }
