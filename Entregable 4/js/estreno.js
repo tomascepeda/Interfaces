656bdd8417f98eb7ffe1bcdee37c56552e0d899c
tomascepeda <tomascepeda15@gmail.com>
2022-11-11 22:58:37 -0300

    Animacon de seccion historia de juego y sprites

--- Entregable 4/js/estreno.js
+++ Entregable 4/js/estreno.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
         document.querySelector("section.reserve").scrollIntoView({ behavior: 'smooth' });
     });
     animateCards();
+    animateLeftScroll();
     let charactersTitle = document.querySelector("#characters-title");
     scrollAnimation(charactersTitle, 500, (percentage) => {
         charactersTitle.style.opacity = percentage;
@@ -13,6 +14,56 @@ document.addEventListener("DOMContentLoaded", () => {
         charactersTitle.style.top = -90 + (percentage * 90) + "px";
     });
 });
+
+function animateLeftScroll() {
+    let h1s = document.querySelectorAll(".scroll-animated-right-column h1");
+    let images = document.querySelectorAll(".scroll-animated-left-column img");
+    h1s.forEach((h1, i) => {
+        let isFirstImage = i == 0;
+        let isLastImage = i == h1s.length -1;
+        let phaseOneFinish = 0.3;
+        let phaseThreeStart = 0.7;
+        let isOnPhaseOne = (percentage) => percentage <= phaseOneFinish;
+        let isOnPhaseThree = (percentage) => percentage >= phaseThreeStart;
+        let scrollValue = window.innerHeight + (window.innerHeight/3);
+        if (isFirstImage)
+            scrollAnimation(h1, scrollValue, (percentage) => {
+                images[i].style.opacity = 1;
+                images[i].style.left = 0;
+                if (isOnPhaseOne(percentage)) {
+                    let percentagePhaseOne = ((percentage * 100) / phaseOneFinish) / 100;
+                    images[i].style.opacity = percentagePhaseOne;
+                    images[i].style.left = percentage <= phaseOneFinish ? -120 + (percentagePhaseOne * 120) + "%" : "0";
+                } else if (isOnPhaseThree(percentage)) {
+                    let percentagePhaseThree = (((percentage - phaseThreeStart) * 100) / phaseOneFinish) / 100;
+                    images[i].style.opacity = 1 - percentagePhaseThree;
+                }
+            });
+        else if (isLastImage)
+            scrollAnimation(h1, scrollValue, (percentage) => {
+                if (percentage <= phaseThreeStart) {
+                    let percentage2 = ((percentage * 100) / phaseOneFinish) / 100;
+                    images[i].style.opacity = percentage2;
+                    images[i].style.left = 0;
+                } else {
+                    let percentage2 = (((percentage - phaseThreeStart) * 100) / phaseOneFinish) / 100;
+                    images[i].style.opacity = 1 - percentage2;
+                    images[i].style.left = (percentage2 * -120) + "%";
+                }
+            });
+        else
+            scrollAnimation(h1, scrollValue, (percentage) => {
+                images[i].style.opacity = 1;
+                if (isOnPhaseOne(percentage)) {
+                    let percentagePhaseOne = ((percentage * 100) / phaseOneFinish) / 100;
+                    images[i].style.opacity = percentagePhaseOne;
+                } else if (isOnPhaseThree(percentage)) {
+                    let percentagePhaseThree = (((percentage - phaseThreeStart) * 100) / phaseOneFinish) / 100;
+                    images[i].style.opacity = 1 - percentagePhaseThree;
+                }
+            });
+    });
+}
 
 function animateCards() {
     let cards = document.querySelectorAll(".section-cards-info .card");
